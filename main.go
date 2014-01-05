274b4b56f771a1e3dd1b362f5fec0ee156ecdfe1
skyblue <ssx205@gmail.com>
2014-01-05 16:01:49 +0800

    save works

--- main.go
+++ main.go
@@ -3,21 +3,61 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"sync"
+	"time"
 
 	"github.com/codegangsta/martini"
+	"github.com/gorilla/websocket"
 )
 
+var (
+	mu     = &sync.Mutex{}
+	stdout = ""
+)
+
+func runCommand(cmd string, args ...string) chan string {
+	ch := make(chan string)
+	go func() {
+		time.Sleep(time.Second * 1)
+
+	}()
+	return ch
+}
 func main() {
 	fmt.Println("Hello World!")
+
 	m := martini.Classic()
 
-	m.Get("/:name", func(params martini.Params) string {
-		return "Hello " + params["name"]
+	// websocket
+	m.Get("/websocket", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("http websocket request")
+		ws, err := websocket.Upgrade(w, r, nil, 1000, 1000)
+		if _, ok := err.(websocket.HandshakeError); ok {
+			http.Error(w, "Not a websocket handshake", 400)
+			return
+		}
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		for {
+			type Message struct {
+				Error error  `json:"error"`
+				Data  string `json:"data"`
+			}
+			msg := new(Message)
+			msg.Data = "hello"
+			ws.WriteJSON(msg)
+			messageType, p, err := ws.ReadMessage()
+			fmt.Println(messageType, string(p))
+			if err != nil {
+				log.Println("bye")
+				return
+			}
+		}
 	})
-
-	//m.Get("/", func() string {
-	//	return "this is index.html"
-	//})
 
 	m.Get("/posts/:id", func(params martini.Params) string {
 		id := params["id"]
--- public/site.js
+++ public/site.js
@@ -1,18 +1,36 @@
 /* site.js */
 
-var wsChars = ['-', '\\', '|', '/'];
-wsIndex = 0;
-var $waiting = $("span.waiting");
-var $console = $("span.console");
-
-var wsId = setInterval(function(){
-	$waiting.html(wsChars[wsIndex]);
-	wsIndex = (wsIndex + 1) % 4;
+$(function(){
+	var pgChars = ['-', '\\', '|', '/'];
+	pgIndex = 0;
+	var $waiting = $("span.waiting");
+	var $console = $("span.console");
 	
-	$console.html($console.html() + "good\n");
-}, 200);
-
-setTimeout(function(){
-	clearInterval(wsId);
-	$waiting.html("");
-}, 2000);
+	var wsUri ="ws://localhost:3000/websocket"; 
+	var ws =new WebSocket(wsUri);
+	ws.onopen = function(e){
+		console.log("open");
+		console.log(e);
+	}
+	ws.onmessage = function(e){
+		console.log(e.data);
+	}
+	ws.onclose = function(e) {
+		console.log("close")
+	}
+	ws.onerror = function(e) {
+		console.log("error")
+	}
+	
+	var pgId = setInterval(function(){
+		$waiting.html(pgChars[pgIndex]);
+		pgIndex = (pgIndex + 1) % 4;
+		
+		$console.html($console.html() + "good\n");
+	}, 200);
+	
+	setTimeout(function(){
+		clearInterval(pgId);
+		$waiting.html("");
+	}, 2000);
+});
